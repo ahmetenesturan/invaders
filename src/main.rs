e60e6ea348b1f544f8b112c3346fa233eba324c5
Ahmet Enes Turan <ahmetenesturan@gmail.com>
2023-07-10 18:02:21 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use std::sync::mpsc;
+use std::{thread, io};
+use std::time::Duration;
+use std::{error::Error, io::stdout};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{self, EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{ExecutableCommand, event};
+use crossterm::cursor::{Hide, Show};
+use invaders::frame::new_frame;
+use invaders::{frame, render};
+//use rusty_audio::Audio;
+
+fn main() -> Result <(), Box<dyn Error>> {
+    
+    //terminal
+    let mut stdout = stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    //render loop in seperate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv(){
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    //gameloop
+    'gameloop: loop {
+        //per-frame init
+        let curr_frame = new_frame();
+        //input
+        while event::poll(Duration::default())?{
+            if let Event::Key(key_event) = event::read()?{
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => break 'gameloop,
+                    _ => {}
+                }
+            }
+        }
+
+        //draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+
+    }
+
+    //cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
+}
